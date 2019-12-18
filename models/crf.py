08426d82f03c3efc58447c3c6f7279a12cba3d8e
Mei <38374566+zmmzGitHub@users.noreply.github.com>
2019-12-18 20:31:26 +0800

    Update crf.py

--- models/crf.py
+++ models/crf.py
@@ -10,7 +10,8 @@ import torch.autograd as autograd
 import torch.nn as nn
 import torch.optim as optim
 import torch.sparse as sparse
-import utils as utils
+
+import models.utils as utils
 
 
 class CRF_L(nn.Module):    # large CRF
